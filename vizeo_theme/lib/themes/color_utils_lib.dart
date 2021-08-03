e180a72d05d93531b43aa43cb806271c12224b7e
Maxvyr <maxvy_@naver.com>
2021-08-03 11:37:07 +0200

    add print !!!!!!

--- vizeo_theme/lib/themes/color_utils_lib.dart
+++ vizeo_theme/lib/themes/color_utils_lib.dart
@@ -1,17 +1,21 @@
 part of '../vizeo_theme.dart';
 
 class ColorUtils {
-  static Color colorStyle(BuildContext context,VzTextType vzTextType) {
+  static Color colorStyle(BuildContext context, VzTextType vzTextType) {
     Color? color;
 
+    print("Enter Switch");
     switch (vzTextType) {
       case VzTextType.typePrimary:
+        print("Primaire");
         color = Theme.of(context).textTheme.bodyText1!.color;
         break;
       case VzTextType.typeSecondary:
+        print("Secondaire");
         color = Theme.of(context).textTheme.headline6!.color;
         break;
       case VzTextType.typeTertiary:
+        print("Terciaire");
         color = Theme.of(context).textTheme.headline5!.color;
         break;
       default:
