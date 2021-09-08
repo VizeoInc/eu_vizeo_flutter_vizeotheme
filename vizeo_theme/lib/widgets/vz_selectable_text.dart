2d591e4137b192c1dcb028ec81f90278782720c5
Maxvyr <maxvy_@naver.com>
2021-09-08 14:10:39 +0200

    :recycle: update name VzText & VzTextSelectable

--- vizeo_theme/lib/widgets/vz_selectable_text.dart
+++ vizeo_theme/lib/widgets/vz_selectable_text.dart
@@ -1,126 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:vizeo_theme/vizeo_theme.dart';
 import 'package:vizeo_theme/private/enum.dart';
 import 'package:vizeo_theme/private/color_utils_lib.dart';
 
-class VzTextSelectable extends StatefulWidget {
-  final String data;
-  final Function()? onTap;
-  final double height;
-  final Color colorShadow;
-  final Offset offset;
-  final double blurRadius;
-  final double fontSize;
-  final FontStyle fontStyle;
-  final FontWeight fontWeight;
-  final TextAlign align;
-  final double textScaleFactor;
-  final bool enableInteractiveSelection;
-  final int maxLines;
-  final Color? color;
-  late final VzTextType _vzTextType;
-
-  VzTextSelectable.typePrimary({
-    required this.data,
-    this.onTap,
-    this.height: 1,
-    this.enableInteractiveSelection = true,
-    this.colorShadow: VzColor.transparent,
-    this.offset = const Offset(1, 1),
-    this.blurRadius = 0.0,
-    this.fontSize: 24.0,
-    this.fontStyle: FontStyle.normal,
-    this.fontWeight = FontWeight.w800,
-    this.align: TextAlign.left,
-    this.textScaleFactor: 1,
-    this.maxLines: 1,
-    this.color,
-  }) {
-    _vzTextType = VzTextType.typePrimary;
-  }
-
-  VzTextSelectable.typeSecondary({
-    required this.data,
-    this.onTap,
-    this.height: 1,
-    this.enableInteractiveSelection = true,
-    this.colorShadow: VzColor.transparent,
-    this.offset = const Offset(1, 1),
-    this.blurRadius = 0.0,
-    this.fontSize: 18.0,
-    this.fontStyle: FontStyle.normal,
-    this.fontWeight = FontWeight.w700,
-    this.align: TextAlign.left,
-    this.textScaleFactor: 1,
-    this.maxLines: 1,
-    this.color,
-  }) {
-    _vzTextType = VzTextType.typeSecondary;
-  }
-
-  VzTextSelectable.typeTertiary({
-    required this.data,
-    this.onTap,
-    this.height: 1,
-    this.enableInteractiveSelection = true,
-    this.colorShadow: VzColor.transparent,
-    this.offset = const Offset(1, 1),
-    this.blurRadius = 0.0,
-    this.fontSize: 18.0,
-    this.fontStyle: FontStyle.normal,
-    this.fontWeight = FontWeight.w400,
-    this.align: TextAlign.left,
-    this.textScaleFactor: 1,
-    this.maxLines: 1,
-    this.color,
-  }) {
-    _vzTextType = VzTextType.typeTertiary;
-  }
-
-  @override
-  _VzTextSeletableState createState() => _VzTextSeletableState();
-}
-
-class _VzTextSeletableState extends State<VzTextSelectable> {
-  @override
-  Widget build(BuildContext context) {
-    return SelectableText(
-      widget.data,
-      textAlign: widget.align,
-      textScaleFactor: widget.textScaleFactor,
-      maxLines: widget.maxLines,
-      enableInteractiveSelection: widget.enableInteractiveSelection,
-      onTap: widget.onTap,
-      toolbarOptions: const ToolbarOptions(
-        copy: true,
-        selectAll: true,
-      ),
-      style: TextStyle(
-        color: widget.color ?? ColorUtils.colorStyle(widget._vzTextType),
-        fontWeight: widget.fontWeight,
-        fontSize: widget.fontSize,
-        fontStyle: widget.fontStyle,
-        height: widget.height,
-        shadows: [
-          Shadow(
-            color: widget.colorShadow,
-            offset: widget.offset,
-            blurRadius: widget.blurRadius,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class VzTextSelectableV2 extends SelectableText {
+class VzTextSelectable extends SelectableText {
   String data;
   final int maxlines;
   final bool enableInteractiveSelection;
   final Function()? onTap;
   final Color? color;
 
-  VzTextSelectableV2.typePrimary({
+  VzTextSelectable.typePrimary({
     required this.data,
     TypeDeTextStyle style = TypeDeTextStyle.body1,
     this.onTap,
@@ -141,7 +30,7 @@ class VzTextSelectableV2 extends SelectableText {
           ),
         );
 
-  VzTextSelectableV2.typeSecondary({
+  VzTextSelectable.typeSecondary({
     required this.data,
     TypeDeTextStyle style = TypeDeTextStyle.body1,
     this.onTap,
@@ -162,7 +51,7 @@ class VzTextSelectableV2 extends SelectableText {
           ),
         );
 
-  VzTextSelectableV2.typeTertiary({
+  VzTextSelectable.typeTertiary({
     required this.data,
     TypeDeTextStyle style = TypeDeTextStyle.body1,
     this.onTap,
--- vizeo_theme/lib/widgets/vz_text.dart
+++ vizeo_theme/lib/widgets/vz_text.dart
@@ -1,109 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:vizeo_theme/vizeo_theme.dart';
 import 'package:vizeo_theme/private/enum.dart';
 import 'package:vizeo_theme/private/color_utils_lib.dart';
 
-class VzText extends StatefulWidget {
-  final String data;
-  final double height;
-  final Color colorShadow;
-  final Offset offset;
-  final double blurRadius;
-  final double fontSize;
-  final FontStyle fontStyle;
-  final FontWeight fontWeight;
-  final TextAlign align;
-  final double textScaleFactor;
-  final int maxLines;
-  final Color? color;
-  late final VzTextType _vzTextType;
-
-  VzText.typePrimary(
-      {required this.data,
-      this.height = 1,
-      this.colorShadow = VzColor.transparent,
-      this.offset = const Offset(1, 1),
-      this.blurRadius = 0.0,
-      this.fontSize = 24.0,
-      this.fontStyle = FontStyle.normal,
-      this.fontWeight = FontWeight.w800,
-      this.align = TextAlign.left,
-      this.textScaleFactor = 1,
-      this.maxLines = 1,
-      this.color}) {
-    _vzTextType = VzTextType.typePrimary;
-  }
-
-  VzText.typeSecondary(
-      {required this.data,
-      this.height = 1,
-      this.colorShadow = VzColor.transparent,
-      this.offset = const Offset(1, 1),
-      this.blurRadius = 0.0,
-      this.fontSize = 18.0,
-      this.fontStyle = FontStyle.normal,
-      this.fontWeight = FontWeight.w700,
-      this.align = TextAlign.left,
-      this.textScaleFactor = 1,
-      this.maxLines = 1,
-      this.color}) {
-    _vzTextType = VzTextType.typeSecondary;
-  }
-
-  VzText.typeTertiary(
-      {required this.data,
-      this.height = 1,
-      this.colorShadow = VzColor.transparent,
-      this.offset = const Offset(1, 1),
-      this.blurRadius = 0.0,
-      this.fontSize = 16.0,
-      this.fontStyle = FontStyle.normal,
-      this.fontWeight = FontWeight.w400,
-      this.align = TextAlign.left,
-      this.textScaleFactor = 1,
-      this.maxLines = 1,
-      this.color}) {
-    _vzTextType = VzTextType.typeTertiary;
-  }
-
-  @override
-  _VzTextState createState() => _VzTextState();
-}
-
-class _VzTextState extends State<VzText> {
-  @override
-  Widget build(BuildContext context) {
-    var colorTheme = widget.color ?? ColorUtils.colorStyle(widget._vzTextType);
-    return Text(
-      widget.data,
-      textAlign: widget.align,
-      textScaleFactor: widget.textScaleFactor,
-      maxLines: widget.maxLines,
-      style: TextStyle(
-        color: colorTheme,
-        fontWeight: widget.fontWeight,
-        fontSize: widget.fontSize,
-        fontStyle: widget.fontStyle,
-        height: widget.height,
-        shadows: [
-          Shadow(
-            color: widget.colorShadow,
-            offset: widget.offset,
-            blurRadius: widget.blurRadius,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class VzTextV2 extends Text {
+class VzText extends Text {
   late String data;
   late final int maxlines;
   late final TypeDeTextStyle type;
   late final Color? color;
 
-  VzTextV2.typePrimary({
+  VzText.typePrimary({
     required this.data,
     TypeDeTextStyle style = TypeDeTextStyle.body1,
     this.maxlines = 10,
@@ -116,7 +21,7 @@ class VzTextV2 extends Text {
           ),
         );
 
-  VzTextV2.typeSecondary({
+  VzText.typeSecondary({
     required this.data,
     TypeDeTextStyle style = TypeDeTextStyle.body1,
     this.maxlines = 10,
@@ -129,7 +34,7 @@ class VzTextV2 extends Text {
           ),
         );
 
-  VzTextV2.typeTertiary({
+  VzText.typeTertiary({
     required this.data,
     TypeDeTextStyle style = TypeDeTextStyle.body1,
     this.maxlines = 10,
