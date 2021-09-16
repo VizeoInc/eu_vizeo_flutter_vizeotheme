943e22cb255105933cf55cf5078e1e8c8491b65a
Maxvyr <maxvid30@gmail.com>
2021-09-16 11:21:49 +0200

    :adhesive_bandage: add possible gestion TextStyle independante for enum TypeDeTextStyle

--- vizeo_theme/lib/widgets/vz_text.dart
+++ vizeo_theme/lib/widgets/vz_text.dart
@@ -3,35 +3,37 @@ import 'package:vizeo_theme/private/enum.dart';
 import 'package:vizeo_theme/private/color_utils_lib.dart';
 
 class VzText extends Text {
-  late String data;
+  late final String data;
   late final int maxlines;
-  late final TypeDeTextStyle type;
-  late final Color? color;
+  final TextStyle? textStyleCustom;
+  final Color? color;
 
   VzText.typePrimary({
     required this.data,
     TypeDeTextStyle style = TypeDeTextStyle.body1,
     this.maxlines = 10,
     this.color,
-  }) : super(
-          data,
-          maxLines: maxlines,
-          style: style.textStyle.copyWith(
-            color: color ?? ColorUtils.colorStyle(VzTextType.typePrimary),
-          ),
-        );
+    this.textStyleCustom,
+  }) : super(data,
+            maxLines: maxlines,
+            style: textStyleCustom ??
+                style.textStyle.copyWith(
+                  color: color ?? ColorUtils.colorStyle(VzTextType.typePrimary),
+                ));
 
   VzText.typeSecondary({
     required this.data,
     TypeDeTextStyle style = TypeDeTextStyle.body1,
     this.maxlines = 10,
     this.color,
+    this.textStyleCustom,
   }) : super(
           data,
           maxLines: maxlines,
-          style: style.textStyle.copyWith(
-            color: color ?? ColorUtils.colorStyle(VzTextType.typeSecondary),
-          ),
+          style: textStyleCustom ??
+              style.textStyle.copyWith(
+                color: color ?? ColorUtils.colorStyle(VzTextType.typeSecondary),
+              ),
         );
 
   VzText.typeTertiary({
@@ -39,11 +41,13 @@ class VzText extends Text {
     TypeDeTextStyle style = TypeDeTextStyle.body1,
     this.maxlines = 10,
     this.color,
+    this.textStyleCustom,
   }) : super(
           data,
           maxLines: maxlines,
-          style: style.textStyle.copyWith(
-            color: color ?? ColorUtils.colorStyle(VzTextType.typeTertiary),
-          ),
+          style: textStyleCustom ??
+              style.textStyle.copyWith(
+                color: color ?? ColorUtils.colorStyle(VzTextType.typeTertiary),
+              ),
         );
 }
