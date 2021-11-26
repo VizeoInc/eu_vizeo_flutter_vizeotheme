5cd63c7e92d6f9bb5d4e5e17f3ed13ceeaae8431
Maxvyr <maxvid30@gmail.com>
2021-11-26 10:55:12 +0100

    add 2 function to Widget

--- vizeo_theme/lib/src/widgets/vz_widget_utils.dart
+++ vizeo_theme/lib/src/widgets/vz_widget_utils.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../vizeo_theme.dart';
 
 extension MiseEnForme on Widget {
   Padding withPaddingWidget({
@@ -26,4 +29,23 @@ extension MiseEnForme on Widget {
       child: this,
     );
   }
+
+  Center withCenter() {
+    return Center(
+      child: this,
+    );
+  }
+
+ Future<dynamic> showHimself(){
+ return showDialog(
+      context: vzThemeKey.currentContext!,
+      builder: (context) {
+        return SimpleDialog(
+          backgroundColor: Colors.transparent,
+          children: [
+            this,
+          ],
+        );
+      });
+}
 }
