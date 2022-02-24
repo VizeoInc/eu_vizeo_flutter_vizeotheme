4ce4483675b018dc0f305c93558a77aef32e4d26
maxvyr <maxvid30@gmail.com>
2022-02-24 15:11:53 +0100

    :heavy_plus_sign: add package flemme

--- vizeo_theme/lib/src/widgets/vz_widget_utils.dart
+++ vizeo_theme/lib/src/widgets/vz_widget_utils.dart
@@ -1,55 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:vizeo_theme/src/private/vizeo_radius.dart';
+import 'package:flemme/flemme.dart';
 
 import '../../vizeo_theme.dart';
 
 extension MiseEnForme on Widget {
-  Padding withPaddingWidget({
-    EdgeInsets padding = const EdgeInsets.all(8.0),
-  }) {
-    return Padding(padding: padding, child: this);
-  }
-
-  Flexible withFlexibleWidget({
-    int flex = 1,
-    FlexFit fit = FlexFit.tight,
-  }) {
-    return Flexible(
-      flex: flex,
-      fit: fit,
-      child: this,
-    );
-  }
-
-  Expanded withExpandedWidget({
-    int flex = 1,
-  }) {
-    return Expanded(
-      flex: flex,
-      child: this,
-    );
-  }
-
-  Center withCenter() {
-    return Center(
-      child: this,
-    );
-  }
-
-  Future<dynamic> showHimself() {
-    return showDialog(
-      context: vzThemeKey.currentContext!,
-      builder: (context) {
-        return SimpleDialog(
-          backgroundColor: Colors.transparent,
-          children: [
-            this,
-          ],
-        );
-      },
-    );
-  }
-
   Container withContainerTitle(String title) {
     return Container(
       decoration: BoxDecoration(
@@ -72,10 +27,10 @@ extension MiseEnForme on Widget {
               data: title,
               style: TypeDeTextStyle.body1,
               color: VzColor.textPrimary(isReverse: true),
-            ).withPaddingWidget().withCenter(),
+            ).withPadding().withCenter(),
           ),
           // ignore: unnecessary_this
-          this.withPaddingWidget().withFlexibleWidget(),
+          this.withPadding().withFlexible(),
         ],
       ),
     );
